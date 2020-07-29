e471b69ddc14ab64802f96124ba2c2916cf89a1e
cyber-lama <dimmeln1k@yandex.ru>
2020-07-30 01:47:42 +0300

    - Добавил vue

--- App/Controllers/CommentsController.php
+++ App/Controllers/CommentsController.php
@@ -10,6 +10,28 @@ class CommentsController extends SimpleController
 {
     public function index()
     {
-        return 'Вы на странице для вывода всех комментариев';
+        return <<<HTML
+                
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Vue</title>
+    <link rel="stylesheet" href="public/css/index.css">
+</head>
+<body>
+    <div id="app">
+            <h1>{{ product }}</h1>
+    </div>
+</body>
+<script src="public/js/index.js"></script>
+<script>
+
+</script>
+</html>
+HTML;
     }
 }
